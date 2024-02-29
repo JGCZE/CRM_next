451f0920889d3ac105c44f86a534737b472e21cd
Jan Gross <jan.gross.cze@gmail.com>
2024-02-29 19:40:32 +0100

    test 2

--- src/app/lib/utils.js
+++ src/app/lib/utils.js
@@ -5,12 +5,14 @@ const connection = {}
 
 export const connectToDb = async () => {
   try {
-    // if (connection.isConnected){
-    //   console.log("using existing connection");
-    //   return  
-    // }
-    const db = await mongoose.connect(process.env.MONGO)
-    connection.isConnected = db.connections[0].readyState;
+    if (connection.isConnected){
+      console.log("using existing connection");
+      return  
+    }
+    await mongoose.connect(process.env.MONGO)
+      .then(() => console.log("connected to db"))
+      .catch((err) => console.log(err));
+    //connection.isConnected = db.connections[0].readyState;
   } catch (error) {
     console.log(error);
     throw new Error("This is a fucking error bro...!?");
