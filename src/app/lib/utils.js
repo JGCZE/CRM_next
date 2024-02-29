ea02e072e7d85bc73c4a680f25aa6c2975a0ebb0
Jan Gross <jan.gross.cze@gmail.com>
2024-02-29 18:50:23 +0100

    connection duration

--- src/app/lib/utils.js
+++ src/app/lib/utils.js
@@ -9,7 +9,12 @@ export const connectToDb = async () => {
       console.log("using existing connection");
       return  
     }
-    const db = await mongoose.connect(process.env.MONGO);
+    const db = await mongoose.connect(process.env.MONGO, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 25000
+    
+    });
     connection.isConnected = db.connections[0].readyState;
   } catch (error) {
     console.log(error);
