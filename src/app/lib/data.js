4f693694cf0e32ac2ef638a1fabe147f41d73dc3
Jan Gross <jan.gross.cze@gmail.com>
2024-03-01 13:05:31 +0100

    final

--- src/app/lib/data.js
+++ src/app/lib/data.js
@@ -2,17 +2,17 @@ import { connectToDb } from "./utils";
 import { Client } from "./models";
 import { unstable_noStore as noStore } from "next/cache";
 
-// export const getClient = async () => {
-//   try {
-//     connectToDb()
-//     const client = await Client.find()
-//     return client;
+export const getClient = async () => {
+  try {
+    connectToDb()
+    const client = await Client.find()
+    return client;
 
-//   } catch (error) {
-//     console.log(error);
-//     throw new Error("FAIL from getClient")
-//   }
-// }
+  } catch (error) {
+    console.log(error);
+    throw new Error("FAIL from getClient")
+  }
+}
 
 export const getOneClient = async (id) => {
   noStore()
