07c27d7cdc8efcab281d9c62bcca3542f57297a0
Jan Gross <jan.gross@regiojet.cz>
2024-03-25 15:52:18 +0100

    login design

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,12 +1,18 @@
+import { Button } from "@radix-ui/themes";
 import { login } from "../../lib/actions";
+import styles from "./LoginPage.module.css";
+import { ArrowTopRightIcon } from '@radix-ui/react-icons'
 
 const LoginPage = async () => {
   return (
-    <div>
-      <form action={login}>
+    <div className={styles.container}>
+      <form action={login} className={styles.form}>
           <input type="text" placeholder="user name" name="username"/>
           <input type="password" placeholder="password" name="password"/>
-          <button>Login</button>
+          <Button variant="classic" color="indigo">
+             Login
+             <ArrowTopRightIcon />
+          </Button>
       </form>
     </div>
   )
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/Navbar/Navbar'
-import login from "./lib/auth";
 
 const inter = Inter({ subsets: ['latin'] })
 
